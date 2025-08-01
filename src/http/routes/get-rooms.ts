ea04dcedf997bb647ba95df3dcfcb5432fa634ee
matheuskrol <matheuskrol27@gmail.com>
2025-08-01 17:14:55 -0300

    feat: add create question, create room and get room questions

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,15 +1,20 @@
-import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { db } from "../../db/connection.ts";
 import { schema } from "../../db/schema/index.ts";
+import { count, eq } from "drizzle-orm";
 
-export const getRoomsRoute: FastifyPluginAsyncZod = async (app) => {
+export const getRoomsRoute: FastifyPluginCallbackZod = async (app) => {
   app.get("/rooms", async () => {
     const results = await db
       .select({
         id: schema.rooms.id,
         name: schema.rooms.name,
+        createdAt: schema.rooms.createdAt,
+        questionsCount: count(schema.questions.id),
       })
       .from(schema.rooms)
+      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
+      .groupBy(schema.rooms.id)
       .orderBy(schema.rooms.createdAt);
     return results;
   });
